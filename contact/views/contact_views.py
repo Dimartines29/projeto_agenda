bf87373d10b62ed119b0aead8292aca0d3ca4811
Diego <diegomotta49@hotmail.com>
2024-05-26 19:57:05 -0300

    criando partials

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -5,14 +5,16 @@ from contact.models import Contact
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
 
-    ctx = {'contacts': contacts,}
+    ctx = {'contacts': contacts, 'site_title': 'Contatos - '}
 
     return render(request, 'contact/index.html', ctx)
 
 def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
-    ctx = {'contact': single_contact,}
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
+    ctx = {'contact': single_contact, 'site_title': site_title}
 
     return render(request, 'contact/contact.html', ctx)
 
