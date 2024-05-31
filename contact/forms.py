7f4ed829bd2f7a25eddc14e2ae9ca7eb57211977
Diego <diegomotta49@hotmail.com>
2024-05-31 10:29:43 -0300

    Continuação da implementação da criação de um contato.

--- contact/forms.py
+++ contact/forms.py
@@ -3,11 +3,28 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Digite o nome aqui'}), label='Nome', help_text='Digite o primeiro nome do seu contato')
+
     class Meta:
         model = models.Contact
-        fields = ('first_name', 'last_name', 'phone',)
+        fields = ('first_name', 'last_name', 'phone', 'email', 'description', 'category')
 
     def clean(self):
         cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-        self.add_error('first_name', ValidationError('Mensagem de erro', code='invalid'))
+        if first_name == last_name:
+            msg = ValidationError('Nome não pode ser igual ao sobrenome')
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error('first_name', ValidationError('Não digite ABC neste campo'))
+
+        return first_name
